4b500c9c74dfd26b282743940bce3c168adee9fa
Tina Hang <mstinahang@gmail.com>
2020-02-22 19:51:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, iterator) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection);
+      for (let i = 0; i , newCollection.length, i++) 
+        iterator(newCollection[i])
+        return collection
     },
 
     map: function() {
